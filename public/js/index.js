960cdfc875eba4ade35b1e84530d6a514b0ad4c6
XKalden <kaldend.tsering@outlook.com>
2018-08-20 09:05:14 -0400

    added client server emit function

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+
+socket.on('connect', function(){
+    console.log('Connected to server');
+
+
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
+});
+
+// email 
+socket.on('newMessage', function(message) {
+    console.log('New Message', message);
+});
+
+
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 // port 
@@ -7,13 +10,44 @@ const port = process.env.PORT || 3000;
 
 
 var app = express();
+var server = http.createServer(app);
+// websocket server 
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('someone connected');
+
+
+   
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+
+        // emmit function all 
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+    });
+
+
+
+
+    // disconnection function 
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+
+});
+
+
+
 
 app.use(express.static(publicPath));
 
-
-
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server Running!!! ${port}`);
 });
 
