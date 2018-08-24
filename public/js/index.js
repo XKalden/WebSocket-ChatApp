59cb78a5b906222dc2b479cbbb316142f1104b80
XKalden <kaldend.tsering@outlook.com>
2018-08-24 13:06:34 -0400

    auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+  
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+  }
+  
+
 
 socket.on('connect', function(){
     console.log('Connected to server');
@@ -27,8 +44,9 @@ socket.on('newMessage', function(message) {
 
 
 
-
     $('#messages').append(html);
+
+    scrollToBottom();
 
 
     // var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -58,6 +76,8 @@ socket.on('newLocationMessage', function(message){
 
 
     $('#messages').append(html);
+
+    scrollToBottom();
 
     // var li = $('<li></li>');
     // var a = $('<a target="_blank"> My Current Locaiton </a>');
